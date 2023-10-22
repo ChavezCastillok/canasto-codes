4cc7dd65386076a244883e13586d87499aeff6c7
Kevin Chavez Castillo <kachc@outlook.com>
2023-10-22 10:23:15 -0400

    Se agrego el buscador de productos y el diseño, estilo de la pagina con bulma css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,119 @@
 <script>
+  let notiText;
   export let data;
   let { products } = data;
   $: ({ products } = data);
+
+  let search_results = products;
+  let search;
+
+  function search_product(input = "") {
+    let results = [];
+    if (input !== "") {
+      products.forEach((product) => {
+        if (product.description.toLowerCase().includes(input.toLowerCase())) {
+          results.push(product);
+        }
+      });
+    }
+    return results;
+  }
+  function searching() {
+    if (search.length >= 1) {
+      search_results = search_product(search);
+    } else {
+      search_results = products;
+    }
+  }
+
+  function search_reset() {
+    search = "";
+    search_results = products;
+  }
+
+  function SortArray(x, y) {
+    if (x.description.toLowerCase() < y.description.toLowerCase()) {
+      return -1;
+    }
+    if (x.description.toLowerCase() > y.description.toLowerCase()) {
+      return 1;
+    }
+    return 0;
+  }
+
+  function removeNotification() {
+    notiText.style.display = "none";
+  }
 </script>
 
-<table>
-  <tr>
-    <th>code</th>
-    <th>description</th>
-  </tr>
-  {#each products as product}
-    <tr>
-      <td>{String(product.id).padStart(6, "0")}</td>
-      <td>{product.description}</td>
-    </tr>
-  {/each}
-</table>
+<div class="container is-fluid">
+  <header>
+    <h1 class="title">Keviiin</h1>
+  </header>
+  <main class="section is-flex is-justify-content-center">
+    <table class="table is-bordered is-striped">
+      <thead>
+        <tr>
+          <td colspan="2">List of Products without Bar Code</td>
+        </tr>
+        <tr>
+          <td colspan="2">
+            <article class="field">
+              <p class="control has-icons-left">
+                <!-- svelte-ignore a11y-autofocus -->
+                <input
+                  class="input is-primary"
+                  type="text"
+                  name="description"
+                  id="description"
+                  placeholder="write description or part "
+                  bind:value={search}
+                  on:input={searching}
+                  on:click={search_reset}
+                  autofocus
+                />
+                <span class="icon is-left">
+                  <ion-icon name="search-outline" />
+                </span>
+              </p>
+            </article>
+          </td>
+        </tr>
+        <tr>
+          <th>Code</th>
+          <th>Description</th>
+        </tr>
+      </thead>
+      <tbody>
+        {#if search_results.length > 0}
+          {#each search_results as product}
+            <tr>
+              <td>{String(product.id).padStart(6, "0")}</td>
+              <td>{product.description}</td>
+            </tr>
+          {/each}
+        {:else}
+          <div class="notification is-warning" bind:this={notiText}>
+            <button class="delete" on:click={removeNotification} />
+            Avisame para agregar el codigo/producto que hace falta. Atte. Kevin.
+          </div>
+          <!-- {alert("Falta un codigo? Avisame para agregarlos!! Atte. Kevin")} -->
+          <!-- <tr>
+            <td>0x6</td>
+            <td>¿other product? Avisadme! Atte. Kevin</td>
+          </tr> -->
+        {/if}
+      </tbody>
+    </table>
+  </main>
+</div>
+
+<style>
+  @import url("bulma/css/bulma.min.css");
+
+  .notification {
+    position: fixed;
+    top: 10%;
+    right: 10%;
+  }
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,7 +7,13 @@ const config = {
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter({
-      // See below for an explanation of these options
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
+      pages: "build",
+      assets: "build",
+      fallback: undefined,
+      precompress: false,
+      strict: true,
     }),
   },
 };
